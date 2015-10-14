3574a6109d430bdd5b930a2aabe220f74754703d
shiweifu <shiweifu@gmail.com>
2015-10-14 14:40:07 +0800

    Initial Commit

---
+++ DafaWebCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DafaWebCache
+//
+//  Created by shiweifu on 10/14/15.
+//  Copyright (c) 2015 shiweifu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DafaWebCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DafaWebCache
+//
+//  Created by shiweifu on 10/14/15.
+//  Copyright (c) 2015 shiweifu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
